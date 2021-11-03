9b3abd5d928de1b9166e5e20e0034ad9c3338bd4
Francisco Oliveira <fransoliveira@gmail.com>
2021-11-03 10:30:35 -0300

    Requisições App para a Api

--- Alura.WebAPI.WebApp/HttpClients/LivroApiClient.cs
+++ Alura.WebAPI.WebApp/HttpClients/LivroApiClient.cs
@@ -1,7 +1,7 @@
 ﻿using Alura.ListaLeitura.Modelos;
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-
 using Lista = Alura.ListaLeitura.Modelos.ListaLeitura;
 
 namespace Alura.ListaLeitura.HttpClients
@@ -34,13 +34,50 @@ namespace Alura.ListaLeitura.HttpClients
             resposta.EnsureSuccessStatusCode(); //200
         }
 
-
-
         public async Task<Lista> GeListaLeituraAsync(TipoListaLeitura tipo)
         {
             var resposta = await _httpClient.GetAsync($"/api/ListasLeitura/{ tipo}");
             resposta.EnsureSuccessStatusCode(); //200
             return await resposta.Content.ReadAsAsync<Lista>();
         }
+
+
+
+
+
+
+        private string EnvolveComAspasDuplas(string valor)
+        {
+            return $"\"{valor}\"";
+        }
+        private HttpContent CreateMultipartFormDataContent(LivroUpload model)
+        {
+            var content = new MultipartFormDataContent();
+            content.Add(new StringContent(model.Titulo), EnvolveComAspasDuplas("titulo"));
+            content.Add(new StringContent(model.Subtitulo), EnvolveComAspasDuplas("subtitulo"));
+            content.Add(new StringContent(model.Autor), EnvolveComAspasDuplas("autor"));
+            content.Add(new StringContent(model.Resumo), EnvolveComAspasDuplas("resumo"));
+
+            if (model.Capa != null)
+            {
+                var imagemContent = new ByteArrayContent(model.Capa.ConvertToBytes());
+                imagemContent.Headers.Add("content-type", "image/png"); //poderia ser um arquivo (PDF, Excel etc.)
+                content.Add(
+                    imagemContent,
+                    EnvolveComAspasDuplas("capa"), //campo
+                    EnvolveComAspasDuplas("capa.png") //nome do arquivo                  
+                );
+            }
+
+            content.Add(new StringContent(model.Lista.ParaString()), EnvolveComAspasDuplas("lista"));            
+            return content;
+        }
+
+        public async Task PostLivroAsync(LivroUpload model)
+        {
+            HttpContent content = CreateMultipartFormDataContent(model);
+            var resposta = await _httpClient.PostAsync($"/api/Livros", content);
+            resposta.EnsureSuccessStatusCode(); //200
+        }        
     }
 }
